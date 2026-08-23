5e34c746801aac9cb933f0eb62a07b129cc102fb
tocoyashi <tocoyashi@gmail.com>
2026-08-23 22:58:26 +0200

    Decrease TOP_N_COINS from 40 to 32

--- bulls_signals_bot.py
+++ bulls_signals_bot.py
@@ -18,7 +18,7 @@ TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN') or os.environ.get('BOT_TOKEN')
 CHANNEL_ID = os.environ.get('CHANNEL_ID')
 
 TIMEFRAME = '1h'
-TOP_N_COINS = 40
+TOP_N_COINS = 32
 LEVERAGE = 15
 
 # Targets (4 أهداف)
